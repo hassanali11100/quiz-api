032b1f76740ca99af7dabd68d5bf90ed3ffa5448
hassanali11100 <hassanali11100@gmail.com>
2019-09-05 18:01:26 +0500

    feat/answers model added

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_05_081004) do
+ActiveRecord::Schema.define(version: 2019_09_05_115726) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.boolean "is_correct?"
+    t.bigint "question_id", null: false
+    t.bigint "choice_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["choice_id"], name: "index_answers_on_choice_id"
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
 
   create_table "choices", force: :cascade do |t|
     t.text "text"
@@ -39,6 +49,8 @@ ActiveRecord::Schema.define(version: 2019_09_05_081004) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "answers", "choices"
+  add_foreign_key "answers", "questions"
   add_foreign_key "choices", "questions"
   add_foreign_key "questions", "quizzes"
 end
