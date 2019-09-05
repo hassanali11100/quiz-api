ffadcb231d3dba1fcf5a1d0678d299936329b750
hassanali11100 <hassanali11100@gmail.com>
2019-09-05 10:47:23 +0500

    feature/questions test model added

--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class QuestionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @quiz = quizzes(:one)
+  end
+
+  test "should save valid question" do
+    assert_difference('Question.count', 1) do
+      @question = @quiz.questions.new({text: 'This is a test quesiton'})
+      @question.save
+    end
+  end
+
+  test "should not save and invalid question" do
+    assert_difference('Question.count', 0) do
+      @question = @quiz.questions.new({})
+      @question.save
+    end
+  end
 end
