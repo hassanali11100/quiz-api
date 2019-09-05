7ed8c445a151acb0234b8b695c08b60dd606f981
hassanali11100 <hassanali11100@gmail.com>
2019-09-05 18:38:49 +0500

    feat/answers controller layer added

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :quizzes do
     resources :questions do
       resources :choices
+      resources :answers
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
