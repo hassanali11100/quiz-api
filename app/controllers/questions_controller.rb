b8888d8c8708ea98d3d74743bd1f2e08bf211c9f
hassanali11100 <hassanali11100@gmail.com>
2019-09-05 10:33:04 +0500

    feature/questions delete method added

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
     before_action :set_quiz
+    before_action :set_question, only: [:destroy]
 
     def index
         @questions = @quiz.questions
@@ -15,10 +16,18 @@ class QuestionsController < ApplicationController
         end
     end
 
+    def destroy
+        @question.destroy
+    end
+
     private
 
     def set_quiz
         @quiz = Quiz.find(params[:quiz_id])
+    end
+
+    def set_question
+        @question = Question.find(params[:id])
     end
 
     def question_params
