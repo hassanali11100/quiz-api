fc20a57419e2a23d6e6b9c98235e187e087487a0
hassanali11100 <hassanali11100@gmail.com>
2019-09-05 10:33:04 +0500

    feature/questions delete method added

--- test/controllers/questions_controller_test.rb
+++ test/controllers/questions_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class QuestionsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @quiz = quizzes(:one)
+    @question = questions(:one)
   end
 
   test "should get index" do
@@ -11,9 +12,16 @@ class QuestionsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create quesiton" do
-    @question = questions(:one)
     assert_difference('Question.count') do
       post quiz_questions_url(@quiz), params: { text: @question.text }, as: :json
     end
+  end
+
+  test "should delete quesiton" do
+    assert_difference('Question.count', -1) do
+      delete quiz_question_url(@quiz, @question)
+    end
+
+    assert_response 204
   end
 end
